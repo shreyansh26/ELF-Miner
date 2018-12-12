5be859b77d5e139e6095c81433e784ed2c0b7b3a
Shreyansh <shreyansh.pettswood@gmail.com>
2018-12-12 13:14:59 +0530

    Complete end to end pipeline done

--- run_system.py
+++ run_system.py
@@ -3,7 +3,11 @@ import os
 
 PATH = "./elfs"
 
+subprocess.call('rm results.csv', shell=True)
 for i in os.listdir(PATH):
 	file = os.path.join(os.path.join(os.getcwd(), PATH), i)
 	print(file)
 	subprocess.call("python ELFMiner.py " + "'" + file + "'", shell=True)
+
+subprocess.call("mv results.csv system/final.csv", shell=True)
+subprocess.call("cd system && make", shell=True)
